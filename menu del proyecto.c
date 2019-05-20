a192ece58b66c74d79abfc5b85c014e76539e9b1
RedBaronUPM <47811791+RedBaronUPM@users.noreply.github.com>
2019-05-20 19:43:49 +0200

    Update menu del proyecto.c

--- menu del proyecto.c
+++ menu del proyecto.c
@@ -4,8 +4,9 @@
 #define MAX 200
 int main()
 {
-	int opc, i, x, n, j;// opc es la opcion que se elige en el menu principal
-	char msg[MAX], cyph[MAX];
+	int cifrado[10]={5,2,6,3,8,4,9,0,7,1}, inicial[10]={0,1,2,3,4,5,6,7,8,9},i,j,k,l,m,x,n,opc, espacios=0, caracteres_restantes;
+	char msg[MAX], cyph[MAX], v1[10], v2[10];// opc es la opcion que se elige en el menu principal
+	
 	do{
 
 		system("cls");//Borramos todo lo escrito anteriormente
@@ -14,7 +15,7 @@ int main()
 		printf("1.-Menu de encriptacion\n");//este menú contiene los distintos métodos de encriptacion
 		printf("2.-Menu de desencriptacion\n");//este otro contiene las formas en las que se pueden descodificar los mensajes ya encriptados por estos métodos
 		printf("3.-Descripcion de los codigos\n");//este apartado explica al usuario las opciones en las que puede encriptar su mensaje
-		printf("9.-Salir\n");//sales del programa, fin
+		printf("4.-Salir\n");//sales del programa, fin
 		printf("Opcion escogida: ");
 		scanf("%d",&opc);//elige la opcion
 		switch(opc)
@@ -26,10 +27,11 @@ int main()
 				system("cls");
 				printf("MENU DE ENCRIPTACION\n\n");
 				//metodos de encriptacion
-				printf("1.-Metodo Cesar \n");
+				printf("1.-Metodo Cesar\n");
 				printf("2.-Metodo Ascii\n");
-				printf("3.-Encriptacion Alternada\n");
-				printf("4.-Salir\n");//vas al menu principal
+				printf("3.-Metodo Windings\n");
+				printf("4.-Metodo por transposicion\n");
+				printf("5.-Salir\n");//vuelves al menu principal
 				printf("opcion escogida: ");
 				scanf("%d",&opc1);//eliges el codigo
  					switch(opc1)
@@ -106,6 +108,60 @@ int main()
 					    getch();
  						break;
  					case 4:
+ 						printf("Introduzca el mensaje que desa cifrar: \n");
+						scanf ("%200[^\n]", &msg);
+						
+						    for (i=0, j=0; msg[i]!='\0'; i++, j++)
+						    {
+						        if (msg[i]==' ')//Cada vez que nos encontremos un espacio pasamos al siguiente carácter
+						        {
+						            i++;
+						        }
+						        cyph[j]=msg[i];//El texto queda igual con los espacios eliminados
+						    }
+						    caracteres_restantes =10-(j%10);//Se guarda el número de caracteres restantes para completar los grupos de 10
+						    for (i=0;i<caracteres_restantes;i++)//mientras queden caracteres restantes
+						    {
+						        cyph[j]='*';//Se le asigna a cada uno un caracter nulo
+						        j++;
+						    }
+						    for (i=0, k=0;i<j;i++)//Recorremos el mensaje sin espacios
+						    {
+						        v1[k]=cyph[i];//copiamos el carácter en otra cadena
+						        k++;
+						        if (k%10==0)//si es multiplo de 10
+						        {
+						            v1[k]='\0';//se cierra el grupo
+						            for (m=0;m<10;m++)//Recorremos el grupo de 10
+						            {
+						                v2[m]=v1[inicial[m]];//guardamos en otra cadena los caracteres en el orden inicial
+						            }
+						              v2[10]='\0';//
+						            printf("%s", v2);//imprimimos en pantalla el grupo de 1
+						            
+						            k=0;//reiniciamos el recorrido de la primera cadena
+						     
+						        }
+						    }
+						   printf("\n");
+						     for (i=0;i<j;i++)//recorremos el  mensaje
+						    {
+						        v1[k]=cyph[i];//copiamos el carácter original en otra cadena
+						        k++;
+						        if (k%10==0)//si es múltiplo de 10
+						        {
+						            v1[k]='\0';//se cierra el grupo
+						            for (l=0;l<10;l++)//Recorremos el grupo
+						            {
+						                v2[l]=v1[cifrado[l]];//Guardamos el grupo con el orden cifrado
+						            }
+						              v2[10]='\0';//y lo cerramos
+						   
+						            printf("%s ", v2);//imprimimos en pantalla el grupo
+						            
+						            k=0;//reiniciamos la lectura de grupo
+						        }
+						    }   
  						break;
  					case 5:
  						break;
@@ -125,8 +181,9 @@ int main()
 				printf("MENU DE DESENCRIPTACION\n\n");
 				printf("1.-Metodo Cesar\n");
 				printf("2.-Metodo Ascii\n");
-				printf("3.-Forma 1\n");
-				printf("4.-Salir\n");//vuelves al menu principal
+				printf("3.-Metodo Windings\n");
+				printf("4.-Metodo por transposicion\n");
+				printf("5.-Salir\n");//vuelves al menu principal
 				printf("opcion escogida: ");
 				scanf("%d",&opc2);
 		 			switch(opc2)
@@ -204,8 +261,9 @@ int main()
 				printf("MENU DE INFORMACION\n\n");//en este menu se	ofrece informacio de los codigos
 				printf("1.-Metodo Cesar\n");
 				printf("2.-Metodo Ascii\n");
-				printf("3.-Forma 1\n");
-				printf("4.-Salir\n");//vuelves al menu principal
+				printf("3.-Metodo Windings\n");
+				printf("4.-Metodo por transposicion\n");
+				printf("5.-Salir\n");//vuelves al menu principal
 				printf("opcion escogida: ");
 				scanf("%d",&opc3);
 			 		switch(opc3)
